4717c1e8f06665e4ee327a9d6f0e54c2d73f2138
soidid <soidid@gmail.com>
2015-09-10 10:27:41 +0800

    add partyView to reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import candidates from './candidates';
 import counter from './counter';
 import issues from './issues';
+import partyView from './partyView';
 import positionRecords from './positionRecords';
 
 export default combineReducers({
   candidates,
   counter,
   issues,
+  partyView,
   positionRecords
 });
