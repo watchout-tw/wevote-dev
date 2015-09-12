6e10157e63af32a3b99f46c32cc21bdd14056339
soidid <soidid@gmail.com>
2015-09-12 21:48:09 +0800

    (add) candidate position (單一立委，議題表態)

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,14 +1,20 @@
 import { combineReducers } from 'redux';
+import counter from './counter';//// will be removed later
 
 import candidates from './candidates';
-import counter from './counter';
 import issues from './issues';
 
+
+/* For 每個議題表態的三個不同 view */
 import partyView from './partyView';
 import legislatorView from './legislatorView';
 import positionView from './positionView';
 
 import issueController from './issueController';
+
+/* 單一立委的各議題表態 */
+import candidatePositions from './candidatePositions';
+
 
 export default combineReducers({
   candidates,
@@ -19,5 +25,7 @@ export default combineReducers({
   legislatorView,
   positionView,
 
-  issueController
+  issueController,
+
+  candidatePositions
 });
--- src/views/Candidate/Candidate.js
+++ src/views/Candidate/Candidate.js
@@ -3,20 +3,38 @@ import { bindActionCreators } from 'redux';
 import { Link } from "react-router";
 import { connect } from 'react-redux';
 
+import {setCandidateFilter} from '../../ducks/candidatePositions';
+
+
 import CandidateAvatar from '../../components/CandidateAvatar/CandidateAvatar.js';
 
 @connect(
-    state => ({candidates: state.candidates}),
-    dispatch => bindActionCreators({}, dispatch))
+    state => ({candidates: state.candidates,
+               candidatePositions: state.candidatePositions
+               }),
+    dispatch => bindActionCreators({setCandidateFilter}, dispatch))
 
 export default class Candidate extends Component {
+  static propTypes = {
+      setCandidateFilter: PropTypes.func.isRequired
+  }
+  componentWillMount(){
+      const { candidates, setCandidateFilter } = this.props;
+      const id = this.props.params.candidateId;
+      const name = candidates[id].name;
+      setCandidateFilter(name);
+  }
   render() {
     const styles = require('./Candidate.scss');
     const id = this.props.params.candidateId;
-    
+    const {candidatePositions} = this.props;
+
+    console.log(candidatePositions)
+
     return (
       <div className={styles.wrap}>
           <CandidateAvatar id={id} />
+        
       </div>
     );
   }
