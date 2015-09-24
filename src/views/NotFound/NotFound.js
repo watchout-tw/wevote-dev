111b1ee5488d9345712873a72ac1c8da049e26c8
soidid <soidid@gmail.com>
2015-09-24 13:11:45 +0800

    Update 404 text

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 
 export default class NotFound extends Component {
   render() {
+  	const styles = require('./NotFound.scss');
+
     return (
-      <div className="container">
-        <h1>Doh! 404!</h1>
-        <p>These are <em>not</em> the droids you are looking for!</p>
+      <div className={styles.wrap}>
+        <h1>糟糕，是 404 ！</h1>
+        <p>阿草找不到你要的這個頁面～～</p>
       </div>
     );
   }
