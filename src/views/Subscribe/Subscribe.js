63ec0c6f0b270dcb691ab30d80b593a3eeb32dac
soidid <soidid@gmail.com>
2015-10-09 14:54:13 +0800

    Add image to subscribe/succeed

--- src/views/Subscribe/Subscribe.js
+++ src/views/Subscribe/Subscribe.js
@@ -9,7 +9,13 @@ export default class Subscribe extends Component {
     
     let content;
     if(state === "succeed"){
-        content = <div className={styles.text}>訂閱成功！</div>
+        let imageUrl = require("./images/succeed.png")
+        content = 
+        <div>
+          <div className={styles.title}>訂閱成功！</div>
+          <img src={imageUrl} className={styles.img} />
+          <div className={styles.meta}>謝謝你，阿草會認真為你送信。</div>
+        </div>
     }else{//error
         content = <div className={styles.text}>訂閱失敗。<br/>如果您持續遭遇此問題，請與我們聯繫：wevote@watchout.tw</div>
     }
