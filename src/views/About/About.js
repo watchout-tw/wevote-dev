59cacc2dcfe339f83130d35dc25e4d43340a3131
chihaoyo <chihaoyo@gmail.com>
2015-10-09 10:33:08 +0800

    Twist style

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -13,45 +13,54 @@ export default class About extends Component {
 
     let aboutBlock = (
       <div className={styles.content}>
-          <h2>2016，有感投票，為你關心的議題投下一票。</h2>
+          <h2>2016，有感投票<br/>為你關心的議題投下一票</h2>
           <p>這一票，選立委，決定誰能代表台灣人民的最高民意。這一票，決定未來四年每一條法律、每一筆預算，以及台灣人民的未來。</p>
           <p>每四年一次的立委選舉，就像是立委的大型求職博覽會，但身為頭家的人民，卻從來沒看過這些候選人的完整履歷。除了競選標語、宣傳車和那一次又一次的握手，你還能怎麼了解這些立委候選人？</p>
-          <h2>現在，你可以有更好的方式了解他們。</h2>
-          <p>沃草堅持「降低公民參與政治門檻」的初衷，推出「2016立委出任務」，這是一個提供選民了解立委候選人議題表態的網站。我們與許多NGO協力整理過去第八屆立委在立法院中對各重大議題的提案質詢表決，分析出立委及政黨的表態立場；我們也將陸續整理各候選人對相關議題的態度、對推動法案的承諾，提供選民了解候選人的價值理念。</p>
-          <p>我們相信，資訊越充足，越能幫助選民在投票時做出正確的判斷。
-            我們相信，更多的資訊揭露，更好的選舉文化，將是改變台灣民主政治的關鍵因素。</p>
-          <h2>投票一定有風險，立委表現有好有壞，投票前請詳閱「立委出任務」</h2>
-          <h2>We vote, we care.</h2>
-      </div>  
+          <h2>現在，你可以有更好的方式了解他們</h2>
+          <p>沃草堅持「降低公民參與政治門檻」的初衷，推出2016《立委出任務》，這是一個提供選民了解立委候選人議題表態的網站。我們與許多NGO協力整理過去第八屆立委在立法院中對各重大議題的提案質詢表決，分析出立委及政黨的表態立場；我們也將陸續整理各候選人對相關議題的態度、對推動法案的承諾，提供選民了解候選人的價值理念。</p>
+          <p>我們相信，資訊越充足，越能幫助選民在投票時做出正確的判斷。我們相信，更多的資訊揭露，更好的選舉文化，將是改變台灣民主政治的關鍵因素。</p>
+          <div className={styles.reminderCard}>
+            <p>沃草提醒您⋯</p>
+            <h2>投票一定有風險，立委表現有好有壞<br/>投票前請詳閱《立委出任務》</h2>
+            <h2>We vote, we care.</h2>
+          </div>
+      </div>
     );
     let FAQBlock = (
       <div className={styles.content}>
           <FAQ />
-      </div>  
+      </div>
     );
+    //https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Cc-by-nc-sa_euro_icon.svg/1024px-Cc-by-nc-sa_euro_icon.svg.png
+    let ccBadge = require('./images/cc.svg');
     let termsBlock = (
       <div className={styles.content}>
           <div className={styles.licenseBlock}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Cc-by-nc-sa_euro_icon.svg/1024px-Cc-by-nc-sa_euro_icon.svg.png"
+          <img src={ccBadge}
                className={styles.licenseImg} />
           <p>如非特別註明，本站資料及原始碼統一採用
               <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
-                 arget="_blank"
-                 className={styles.link}>創用 CC 姓名標示─非商業性─相同方式分享 4.0 國際 授權條款</a>釋出。</p>
-          <p>需要注意部分如下：</p>
+                arget="_blank"
+                className={`${styles.ia} ${styles.bright}`}>創用CC姓名標示─非商業性─相同方式分享4.0國際授權條款</a>釋出。</p>
+          <p>注意：</p>
           <ol>
-            <li>從立法院網站來源的資料，如立委照片、公報內容等，必須在遵守立法院的<a href="http://www.ly.gov.tw/14_others/copyright.jsp"
-                                                                             target="_blank"
-                                                                             className={styles.link}>著作權規範</a>之下取用。</li>
-            <li>如果在著作權方面有疑慮、建議或是想取得更進一步授權，請來信 wevote@watchout.tw 。</li>
+            <li>從立法院網站來源的資料，如立委照片、公報內容等，必須在遵守立法院的
+              <a href="http://www.ly.gov.tw/14_others/copyright.jsp"
+                target="_blank"
+                className={`${styles.ia} ${styles.bright}`}>著作權規範</a>之下取用。</li>
+            <li>如果在著作權方面有疑慮、建議或是想取得更進一步授權，請來信wevote@watchout.tw。</li>
           </ol>
-          <p>特別感謝：《隸辨》作者厉向晨</p>
+          <p>特別感謝
+            <a href="http://wytype.com/typeface/earlier/"
+              target="_blank"
+              className={`${styles.ia} ${styles.bright}`}>Typeland《隸辨隸書體》</a>
+              作者厉向晨讓我們自由使用《隸辨隸書體》。</p>
           </div>
-      </div>  
+      </div>
     );
 
     let metaData = {};
-    
+
     switch(tabName){
 
       case 'FAQ':
@@ -68,7 +77,7 @@ export default class About extends Component {
             }
           };
           break;
-      
+
       case 'statement':
           mainContent = termsBlock;
           metaData = {
@@ -83,8 +92,8 @@ export default class About extends Component {
             }
           };
           break;
-      
-      default: 
+
+      default:
           mainContent = aboutBlock;
           metaData = {
             title: `關於立委出任務-立委出任務-2016立委投票資訊站`,
@@ -101,7 +110,7 @@ export default class About extends Component {
 
     }
 
-    
+
 
     return (
       <div className={styles.wrap}>
@@ -115,7 +124,7 @@ export default class About extends Component {
           </div>
           {mainContent}
       </div>
-  
+
     );
   }
 }
