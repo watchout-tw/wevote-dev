ca5d41c4aef7f401e5f0119d7351afe5025532a5
chihaoyo <chihaoyo@gmail.com>
2015-10-12 20:59:21 +0800

    Update PeopleProfile+Photo

--- src/components/PeoplePhoto/PeoplePhoto.js
+++ src/components/PeoplePhoto/PeoplePhoto.js
@@ -13,15 +13,15 @@ export default class PeoplePhoto extends Component {
   static propTypes = {
     legislators: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired
-  
+
   }
-  
+
   render () {
 
     const styles = require('./PeoplePhoto.scss');
     const {legislators, id} = this.props;
     const legislator = legislators[id];
-   
+
     let {name, party} = legislator;
     let imgURL;
 
@@ -31,16 +31,15 @@ export default class PeoplePhoto extends Component {
       console.log(e);
       imgURL = require("./images/default.png");
     }
-   
+
     return (
-        <Link to={`/people/${id}/records/`} className={styles["people-pic"]}>
-            <img className={`${styles.avatar}  is-${party}`}
+        <Link to={`/people/${id}/records/`} className={styles.peoplePhoto}>
+            <img className={`is-${party}`}
                  src={imgURL}
                  alt={`${name}-${eng2party_short(party)}立委${name}`} />
         </Link>
-          
+
     );
 
   }
 }
-
--- src/components/PeopleProfile/PeopleProfile.js
+++ src/components/PeopleProfile/PeopleProfile.js
@@ -13,84 +13,74 @@ export default class PeopleProfile extends Component {
   static propTypes = {
     legislators: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired
-  
+
   }
-  
+
   render () {
 
     const styles = require('./PeopleProfile.scss');
 
     const {legislators, id} = this.props;
     const legislator = legislators[id];
-   
+
     let {name, party, partyCht, gender, age, constituency1, constituency2,
          isCandidate, candidateConstituency1, candidateConstituency2} = legislator;
-    
-    
+
+
     //處理選區顯示方式
     if(constituency2 === "N/A"){
         constituency2 = "";
-    
+
     }else{
         constituency2 =  `第${constituency2}選區`;
     }
 
     if(candidateConstituency2 === "N/A"){
         candidateConstituency2 = "";
-    
+
     }else{
         candidateConstituency2 = `第${candidateConstituency2}選區`;
 
     }
-    let ageInfo = <p className={`${styles.infoP} ${styles.engStart}`}>{`${age}歲，${gender}`}</p>;
-    let legislationInfo = <p className={styles.infoP}>{`第8屆 ${constituency1} ${constituency2} 立委`}</p>;
+    let ageInfo = <p>{`${age}歲`}</p>;
+    let legislationInfo = <p>{`第八屆${constituency1}${constituency2}立委`}</p>;
 
- 
-    
+
+
     //處理黨團的資訊顯示方式
     if(name.indexOf("黨團")!== -1){
        ageInfo = "";
-       legislationInfo = <p className={styles.infoP}>第8屆黨團</p>
+       legislationInfo = <p>第八屆黨團</p>
     }
 
     let candidateInfo;
     if(isCandidate === true){
-        candidateInfo = <p className={`${styles.infoP} ${styles.engStart}`}>{` 2016 ${candidateConstituency1} ${candidateConstituency2} 立委候選人`}</p>;
+        candidateInfo = <p>{`2016第九屆${candidateConstituency1}${candidateConstituency2}立委候選人`}</p>;
     }
 
-   
-  
     return (
-        <div className={`$styles["inner-title"] ${styles["people-title"]} `}>
-          <header>
-          <div className={styles["people-basic-info"]}>
-            <div className={styles["people-pic"]}>
-                <PeoplePhoto id={id}/>
-            </div>
-            
-            <div className={styles.nameAndParty}>
-                <Link to={`/people/${id}/records/`}
-                      className={styles.name}>{name}</Link>
+      <div className={styles.wrap}>
+        <header>
+          <div className={styles.peopleProfile}>
+            <div className={styles.avatar}><PeoplePhoto id={id}/></div>
+            <div className={styles.peopleInfo}>
+              <div className={styles.peopleName}>
+                <Link to={`/people/${id}/records/`} className={`${styles.name} ${styles.ia} ${styles.black} ${styles.big}`}>{name}</Link>
                 <div className={styles.party}>
-                  <div className={`${styles["party-flag-s"]} ${styles[party]} ${styles["party-flag"]}`}></div>
-                  <span>{partyCht}</span> 
+                  <div className={`${styles.partyFlag} ${styles.small} ${styles[party]}`}></div>
+                  <span className={styles.partyTitle}>{partyCht}</span>
                 </div>
+              </div>
+              <div className={styles.peopleDetail}>
+                {ageInfo}
+                {legislationInfo}
+                {candidateInfo}
+              </div>
             </div>
-
-            <div className={styles["basic-txt"]}>
-              {ageInfo}
-              {legislationInfo}
-              {candidateInfo}
-            </div>
-           
-
           </div>
-         
-          </header>
-            
-        </div>
+        </header>
+      </div>
     );
 
   }
 }
-
