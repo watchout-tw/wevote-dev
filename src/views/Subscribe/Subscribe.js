e0b241ffef0259cf9b50652c36e0bcc5e5f75315
soidid <soidid@gmail.com>
2015-11-12 16:26:47 +0800

    update subscribe/ view

--- src/views/Subscribe/Subscribe.js
+++ src/views/Subscribe/Subscribe.js
@@ -21,7 +21,7 @@ export default class Subscribe extends Component {
 
   render() {
     const styles = require('./Subscribe.scss');
-    let postcardImg = require('./images/postcard.jpg');
+    let postcardImg = require('./images/postcard.png');
     
     const {showAlert} = this.state;
     let alertStyle = showAlert ? styles.showAlert : styles.hideAlert;
@@ -29,10 +29,9 @@ export default class Subscribe extends Component {
     return (
       <div className={styles.wrap}>
           <div className={styles.innerWrap}>
-             
-              <img src={postcardImg} className={styles.postcardImg} />
-              
 
+              <div className={styles.titleMobile}>想收到新任務的第一手通知嗎？</div>
+              <img src={postcardImg} className={styles.postcardImg} />
               <div className={styles.subscribeContent}>
                   <form
                    action="http://crm.watchout.tw/civicrm/profile/create&amp;gid=10&amp;reset=1"
@@ -40,7 +39,7 @@ export default class Subscribe extends Component {
                    name="Edit"
                    id="Edit">
         
-                     <div className={styles.title}>想收到新任務的第一手通知嗎？</div>
+                     <div className={styles.titleWeb}>想收到新任務的第一手通知嗎？</div>
                      <input name="postURL" type="hidden" value=""/>
                      <input name="cancelURL" type="hidden" value="http://crm.watchout.tw/civicrm/profile?reset=1&amp;gid=10"/>
                      <input name="add_to_group" type="hidden" value="10"/>
@@ -54,7 +53,7 @@ export default class Subscribe extends Component {
                                   ref="emailInput"
                                   className={styles.emailInput}
                                   placeholder="請輸入你的email"/>
-                           <input className={styles.submit}
+                           <input className={styles.submitWeb}
                                   name="_qf_Edit_next"
                                   value="訂閱電子報"
                                   type="submit"
@@ -63,15 +62,18 @@ export default class Subscribe extends Component {
                      </div>
                      
                    </form>
-                   <div className={styles.lawInfo}>
-                     <div className={styles.lawInfoTitle}>最多隔週發信一次，請放心，你隨時可以取消訂閱。</div>
-                     <div className={styles.lawInfoContent}>依據《個人資料保護法》，關於個人資料蒐集、處理及利用聲明：使用本電子報訂閱視為瞭解及同意沃草公司進行您個人資料（email資訊）之蒐集、處理及利用，除非取得您的同意或其他法令之特別規定，絕不會將您的個人資料提供給第三方或使用於電子報訂閱以外之其它用途。您隨時可來信取消此訂閱服務，經取消訂閱後，相關資料立即從訂閱資料庫中刪除。</div>
-                   </div>
-                   
               </div>
-        
+              <input className={styles.submitMobile}
+                     name="_qf_Edit_next"
+                     value="訂閱電子報"
+                     type="submit"
+                     id="_qf_Edit_next"
+                     onClick={this._handleSubmit.bind(this, null)}/>
+              <div className={styles.lawInfo}>
+                <div className={styles.lawInfoTitle}>最多隔週發信一次，請放心，你隨時可以取消訂閱。</div>
+                <div className={styles.lawInfoContent}>依據《個人資料保護法》，關於個人資料蒐集、處理及利用聲明：使用本電子報訂閱視為瞭解及同意沃草公司進行您個人資料（email資訊）之蒐集、處理及利用，除非取得您的同意或其他法令之特別規定，絕不會將您的個人資料提供給第三方或使用於電子報訂閱以外之其它用途。您隨時可來信取消此訂閱服務，經取消訂閱後，相關資料立即從訂閱資料庫中刪除。</div>
+              </div>
 
-              
           </div>
       </div>
   
