84b3f39614b6e946fb2d1589f390ea2658e03b83
soidid <soidid@gmail.com>
2015-12-01 17:40:32 +0800

    add PartyPromise page v1

--- src/views/Parties/Parties.js
+++ src/views/Parties/Parties.js
@@ -109,7 +109,9 @@ export default class Parties extends Component {
               </div>
           </div>
           <div className={styles.story}>
-            黨團衝突戰，將由各地勇者所組成不同的黨團，以團體戰的方式爭奪立法聖殿中的勇者席位。在這場團體戰鬥中，誰能爭取最多席位，同樣考驗島嶼主人的智慧！
+            黨團衝突戰，將由各地勇者所組成不同的黨團，以團體戰的方式爭奪立法聖殿中的勇者席位。在這場團體戰鬥中，誰能爭取最多席位，同樣考驗島嶼主人的智慧！<br/>
+            挑戰任務即刻啟動！<br/>
+            黨團成分分析，快來看誰最適合你？
           </div>
           <div className={styles.actions}>
               <Link to={`/parties-matchgame/`}
--- src/views/Party/Party.js
+++ src/views/Party/Party.js
@@ -9,10 +9,14 @@ import IssueGroup from '../../components/IssueGroup/IssueGroup.js';
 import PositionSquare from '../../components/PositionSquare/PositionSquare.js';
 
 import eng2url from '../../utils/eng2url';
+import eng2cht from '../../utils/eng2cht';
 import parseToPartyPosition from '../../utils/parseToPartyPosition';
 
 /*
-:category => {"records", "info"}
+:category => {"records", "promises", "list"}
+歷史紀錄
+未來承諾
+不分區名單
 */
 
 @connect(
@@ -45,6 +49,9 @@ export default class Party extends Component {
     //政黨基本資料
     const {parties} = this.props;
     const currentParty = parties[id];
+    
+    //content
+    let content;
 
     //SEO
     let title, description;
@@ -53,35 +60,25 @@ export default class Party extends Component {
       case 'records':
         const {partyPositions} = this.state;
         const currentPartyPositions = partyPositions[id];
-        let content;
-        if(currentPartyPositions){
-            //有第八屆的資料
-            const positions = currentPartyPositions.positions;
-            // 改用IssueGroup嗎？
-            content = Object.keys(positions).map((currentIssue, index)=>{
-                //console.log(positions[currentIssue])
-                let issueUrl = eng2url(currentIssue);
-                return (
-                  <Link to={`/parties/${id}/records/${issueUrl}`}
-                    className={styles.issueBlock}
-                    key={index}>
-                    <PositionSquare issueName={currentIssue} data={positions[currentIssue]}/>
-                  </Link>
-                );
-            })
-
+        
+        if(currentPartyPositions){//有第八屆的資料
+            content = <PartyRecords positions={currentPartyPositions.positions} 
+                                    id={id}/>
         }else{
-            content = <div>無過去表態紀錄：該政黨在第八屆立法院沒有席次。</div>
+            content = <div>無歷史表態紀錄：該政黨在第八屆立法院沒有席次。</div>
         }
 
         //SEO
         title = `${currentParty.name}議題表態分析-沃草2016立委出任務`;
         description = `${currentParty.name}對於各項重大議題的戰鬥策略大解析！趕快來看看${currentParty.name}委員在立法院針對下列重大議題講了哪些話！`;
     
-
       break;
 
-      case 'info':
+      case 'promises':
+        content = <PartyPromises id={id} />
+      break;
+
+      case 'list':
         /// TODO
       break;
     }
@@ -105,8 +102,9 @@ export default class Party extends Component {
           <DocumentMeta {...metaData}/>
           <PartyProfile id={id}/>
           <ul className={styles.innerTag}>
-              <li><Link to={`/parties/${id}/records/`} className = { category==="records" ? styles.active : ""}>紀錄</Link></li>
-              <li><Link to={`/parties/${id}/info/`} className = { category==="info" ? styles.active : ""}>資料</Link></li>
+              <li><Link to={`/parties/${id}/records/`} className = { category==="records" ? styles.active : ""}>歷史紀錄</Link></li>
+              <li><Link to={`/parties/${id}/promises/`} className = { category==="promises" ? styles.active : ""}>未來承諾</Link></li>
+              <li><Link to={`/parties/${id}/list/`} className = { category==="list" ? styles.active : ""}>不分區名單</Link></li>
           </ul>
 
           <div className={styles.innerWrap}>
@@ -115,4 +113,87 @@ export default class Party extends Component {
       </div>
     );
   }
+}
+class PartyRecords extends Component {
+    render(){
+        const styles = require('./Party.scss');
+        const {positions, id} = this.props;
+        let result = Object.keys(positions).map((currentIssue, index)=>{
+            //console.log(positions[currentIssue])
+            let issueUrl = eng2url(currentIssue);
+            return (
+              <Link to={`/parties/${id}/records/${issueUrl}`}
+                className={styles.issueBlock}
+                key={index}>
+                <PositionSquare issueName={currentIssue} data={positions[currentIssue]}/>
+              </Link>
+            );
+        })
+
+        return <div>{result}</div>
+
+    }
+}
+function handlePartyPos(value){
+  if(value === "none"){
+    return "尚未回覆"
+  }else{
+    return eng2cht(value);
+  }
+}
+@connect(
+    state => ({
+                 partyPromises: state.partyPromises,
+                 issues: state.issues
+               }),
+    dispatch => bindActionCreators({}, dispatch))
+class PartyPromises extends Component {
+    render(){
+      const styles = require('./Party.scss');
+      const {partyPromises, issues, id} = this.props;
+      const {positions, bills} = partyPromises[id];
+      
+      if(!positions){
+        return <div></div>
+      }
+      let postionItems = Object.keys(positions).map((issueName,i)=>{
+          let pos = positions[issueName].promise.position;
+          let statement = positions[issueName].promise.statement;
+          let issueData = issues[eng2url(issueName)];
+          return (
+              <div className={styles.promiseItem}
+                   key={`promiseItem-${id}-${i}`}>
+                  <div className={styles.promiseTitle}>{eng2cht(issueName)}</div>
+                  <div className={styles.promiseQuestion}>{issueData.question}</div>
+                  <div className={styles.promisePos}>
+                    <div className={`${styles[pos]} ${styles.promiseIcon}`}></div>
+                    <div className={styles.promisePosText}>{handlePartyPos(pos)}</div>
+                  </div>
+                  <div className={styles.promiseStatement}>{statement}</div>
+              </div>
+          )
+      })
+      let billItems = bills.map((value,i)=>{
+          return (
+              <div className={styles.billItem}
+                   key={`billItem-${id}-${i}`}>
+                  <div className={styles.billItemTitle}>{i+1}・{value.goal}</div>
+                  <div>{value.content || "尚未回覆"}</div>
+              </div>
+          )
+      })
+      return (
+        <div>
+          <div className={styles.sectionTitle}>議題表態</div>
+          <div>{postionItems}</div>
+
+          <div className={styles.sectionTitle}>優先法案</div>
+          <div>{billItems}</div>
+          
+          <div className={`${styles.promiseMeta}`}>* 統計更新日期：2015/12/04。
+                <Link className={`${styles.ia} ${styles.bright}`} 
+                      to={`/about/FAQ/`}>我們如何統計的</Link></div>
+        </div>
+      )
+    }
 }
