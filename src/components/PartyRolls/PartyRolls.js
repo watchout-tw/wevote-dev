e3f7f8d1a60c623f7b0c24943cc6d12892349103
soidid <soidid@gmail.com>
2015-12-03 17:56:25 +0800

    refine parties flow v1

---
+++ src/components/PartyRolls/PartyRolls.js
@@ -0,0 +1,140 @@
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import classnames from 'classnames';
+import PKer from '../../components/PKer/PKer';
+
+@connect(
+    state => ({ partyBlock: state.partyBlock }),
+    dispatch => bindActionCreators({}, dispatch))
+export default class PartyRolls extends Component {
+    constructor(props){ super(props)
+        this.state = {
+            focus: "KMT",
+            showMenu: false
+        }
+    }
+    _onChangeFocus(value, event){
+        console.log(value)
+        this.setState({
+            focus: value,
+            showMenu: false
+        })  
+    }
+    _setMenu(value, event){
+        this.setState({
+            showMenu: value
+        }) 
+    }
+    render(){
+      const styles = require("./PartyRolls.scss");
+      const {partyBlock} = this.props;
+      const {focus, showMenu} = this.state;
+      /* current display party */
+      let data = partyBlock[focus];
+      let list = partyBlock[focus].list || [];
+
+      //名單列表
+      let columnItems = list.map((v,i)=>{
+        return (
+          <li>
+            <div className={styles.peopleEntryName}>{v.name}</div>
+            <div className={styles.peopleEntryInfo}>{v.info}</div>
+          </li>
+        )
+      });
+   
+      let menuClasses = classnames({
+        [styles.partyMenuBlock]: true,
+        [styles.show] : showMenu === true
+      })
+      let mobileMenuToggle = (
+          <div>
+              <div className={styles.closeMenu}
+                   onClick={this._setMenu.bind(this, false)}>
+                   <i className="fa fa-times"></i>
+              </div>
+              <div className={styles.toggleMenu}>
+                <div className={styles.partyMenuItem}
+                     onClick={this._setMenu.bind(this, true)}>
+                    <div className={styles.innerHexagon}>
+                      選擇<i className={`fa fa-sort-desc ${styles.toggleIcon}`}></i>
+                    </div>
+                </div>
+              </div>
+          </div>
+      )
+   
+      return (
+          <div className={styles.pbWrap}>
+              {mobileMenuToggle}
+              
+              <div className={menuClasses}>
+                  <div className={`${styles.billboard} ${styles.left}`}>
+                      <PartyMenu side={1} 
+                                 onChangeFocus={this._onChangeFocus.bind(this)}
+                                 focus={focus}/></div>
+                  <div className={`${styles.billboard} ${styles.right}`}>
+                      <PartyMenu side={2} 
+                                 onChangeFocus={this._onChangeFocus.bind(this)}
+                                 focus={focus}/></div>
+              </div>
+          
+              <div className={styles.partyWrap}>
+                  <div className={styles.partyRollTop}>
+                      <h2 className={styles.partyTitle}>
+                          {data.title}參戰名單
+                      </h2>
+                  </div>
+                  <div className={styles.partyRoll}>
+                      <section className={styles.columnBlock}>
+                          <ol>{columnItems}</ol>
+                      </section>
+                  </div> 
+              </div>
+
+          </div>
+      )
+   }
+}
+
+@connect(
+    state => ({
+                parties: state.parties,
+                partyBlock: state.partyBlock
+              }),
+    dispatch => bindActionCreators({}, dispatch))
+
+class PartyMenu extends Component {
+  
+  render(){
+    const {parties, partyBlock, side, onChangeFocus, focus} = this.props;
+    const styles = require("./PartyRolls.scss");
+
+    let side1parties = [], side2parties = [];
+    Object.keys(partyBlock)
+          .map((partyId, index)=>{
+              if(parties[partyId].side === 1){
+                 side1parties.push(parties[partyId])
+              }else{
+                 side2parties.push(parties[partyId])
+              }
+          })
+    let currentSide = (side ===1) ? side1parties : side2parties;
+    let partyMenuItems = currentSide.map((value, index)=>{
+      let active = (focus === value.id);
+      return (
+        <div className={styles.partyMenuItem}
+             key={`side-${side}-${index}`}
+             onClick={onChangeFocus.bind(null,value.id)}>
+             <PKer id={value.id} active={active} />
+        </div>
+      ) 
+    })
+
+    return (
+      <div>{partyMenuItems}</div>
+    )
+  }
+}
