030c6650336fb9104a447cf46bf261a234cfe365
chihaoyo <chihaoyo@gmail.com>
2015-12-06 01:12:22 +0800

    Urgent! Share!

--- src/views/Parties/Parties.js
+++ src/views/Parties/Parties.js
@@ -46,7 +46,7 @@ export default class Parties extends Component {
             showBedge: current
           })
       }
-     
+
 
   }
   componentDidMount(){
@@ -99,9 +99,7 @@ export default class Parties extends Component {
                   <div id="completeBadge"></div>
                   <div className={styles.billComplete}>
                       <img src={missionAccomplishedImg}/>
-                      <div className={shareClasses}>
-                          戰況緊急，揪團參戰。請分享給更多朋友加入！
-                      </div>
+                      <div className={shareClasses}><p className={styles.urgent}>戰況緊急</p><p>按這裡分享，跟你的朋友<br/>一起揪團參戰！</p></div>
                   </div>
               </div>
           )
