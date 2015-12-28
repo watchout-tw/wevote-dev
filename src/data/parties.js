c22f42b5897b83651e2b76fd4deb1903a630b474
soidid <soidid@gmail.com>
2015-12-28 17:11:19 +0800

    update data/parties

--- src/data/parties.js
+++ src/data/parties.js
@@ -1,105 +1,24 @@
 const parties = {
-    "KMT": {
-        "id": "KMT",
-        "name": "中國國民黨",
-        "seats" : 65,
-        "hasBeenCount" : 69,
-        "side" : 2
-    },
     "DPP": {
         "id": "DPP",
         "name": "民主進步黨",
+        "number": "1",
         "seats" : 40,
         "hasBeenCount" : 43,
-        "side" : 1
-    },
-    "TSU": {
-        "id": "TSU",
-        "name": "台灣團結聯盟",
-        "seats" : 3,
-        "hasBeenCount" : 6,
         "side" : 1
     },
     "PFP": {
         "id": "PFP",
         "name": "親民黨",
+        "number": "2",
         "seats" : 2,
         "hasBeenCount" : 4,
-        "side" : 2
-    },
-    "NSU": {
-        "id": "NSU",
-        "name": "無黨團結聯盟",
-        "seats" : 1,
-        "hasBeenCount" : 2,
-        "side" : 2
-    },
-    "MKT": {
-        "id": "MKT",
-        "name": "民國黨",
-        "seats": 1,
-        "hasBeenCount" : 1,
-        "side" : 2
-    },
-    "NPP": {
-        "id": "NPP",
-        "name": "時代力量",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 1
-    },
-    "GSD": {
-        "id": "GSD",
-        "name": "綠黨社會民主黨聯盟",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 1
-    },
-    "NP": {
-        "id": "NP",
-        "name": "新黨",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 2
-    },
-    "MCFAP": {
-        "id": "MCFAP",
-        "name": "軍公教聯盟黨",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 2
-    },
-    "TP": {
-        "id": "TP",
-        "name": "樹黨",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 1
-    },
-    "TIP": {
-        "id": "TIP",
-        "name": "台灣獨立黨",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 1
-    },
-    "FHL": {
-        "id": "FHL",
-        "name": "信心希望聯盟",
-        "seats": 0,
-        "hasBeenCount": 0,
-        "side" : 2
-    },
-    "MPR": {
-        "id": "MPR",
-        "name": "中華民國機車黨",
-        "seats": 0,
-        "hasBeenCount": 0,
         "side" : 2
     },
     "FTP": {
         "id": "FTP",
         "name": "自由台灣黨",
+        "number": "3",
         "seats": 0,
         "hasBeenCount": 0,
         "side" : 1
@@ -107,39 +26,125 @@ const parties = {
     "PPU": {
         "id": "PPU",
         "name": "和平鴿聯盟黨",
+        "number": "4",
         "seats": 0,
         "hasBeenCount": 0,
         "side" : 2 
     },
-    "DASG": {
-        "id": "DASG",
-        "name": "大愛憲改聯盟",
+    "MCFAP": {
+        "id": "MCFAP",
+        "name": "軍公教聯盟黨",
+        "number": "5",
         "seats": 0,
         "hasBeenCount": 0,
-        "side" : 2 
+        "side" : 2
+    },
+    "MKT": {
+        "id": "MKT",
+        "name": "民國黨",
+        "number": "6",
+        "seats": 1,
+        "hasBeenCount" : 1,
+        "side" : 2
+    },
+     "FHL": {
+        "id": "FHL",
+        "name": "信心希望聯盟",
+        "number": "7",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 2
     },
     "CHTY": {
         "id": "CHTY",
+        "number": "8",
         "name": "中華統一促進黨",
         "seats": 0,
         "hasBeenCount": 0,
         "side" : 2 
     },
+     "KMT": {
+        "id": "KMT",
+        "name": "中國國民黨",
+        "number": "9",
+        "seats" : 65,
+        "hasBeenCount" : 69,
+        "side" : 2
+    },
+     "TSU": {
+        "id": "TSU",
+        "name": "台灣團結聯盟",
+        "number": "10",
+        "seats" : 3,
+        "hasBeenCount" : 6,
+        "side" : 1
+    },
+    "NPP": {
+        "id": "NPP",
+        "name": "時代力量",
+        "number": "11",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 1
+    },
+    "DASG": {
+        "id": "DASG",
+        "name": "大愛憲改聯盟",
+        "number": "12",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 2 
+    },
+    "GSD": {
+        "id": "GSD",
+        "name": "綠黨社會民主黨聯盟",
+        "number": "13",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 1
+    },
+    "TIP": {
+        "id": "TIP",
+        "name": "台灣獨立黨",
+        "number": "14",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 1
+    },
+    "NSU": {
+        "id": "NSU",
+        "name": "無黨團結聯盟",
+        "number": "15",
+        "seats" : 1,
+        "hasBeenCount" : 2,
+        "side" : 2
+    },
+    "NP": {
+        "id": "NP",
+        "name": "新黨",
+        "number": "16",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 2
+    },
     "NHSA": {
         "id": "NHSA",
         "name": "健保免費連線",
+        "number": "17",
         "seats": 0,
         "hasBeenCount": 0,
         "side" : 1 
     },
-    "NONE": {
-        "id": "NONE",
-        "name": "無黨籍",
-        "seats" : 0,
-        "hasBeenCount" : 1,
-        "side" : 2
-    }    
-};
+    "TP": {
+        "id": "TP",
+        "name": "樹黨",
+        "number": "18",
+        "seats": 0,
+        "hasBeenCount": 0,
+        "side" : 1
+    }
+}
+
 export default function get(){
     return parties;
 }
