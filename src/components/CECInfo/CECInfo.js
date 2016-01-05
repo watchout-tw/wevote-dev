03459baf57ca365532dc2d49d152f46c99b3399d
soidid <soidid@gmail.com>
2016-01-05 18:27:16 +0800

    refine CEC and Promises layout

--- src/components/CECInfo/CECInfo.js
+++ src/components/CECInfo/CECInfo.js
@@ -5,6 +5,7 @@ import getData from '../../data/getData';
 const { CEC } = getData();
 
 export default class CECInfo extends Component {
+
     render(){
       const styles = require('./CECInfo.scss');
       const {id} = this.props;
@@ -13,19 +14,39 @@ export default class CECInfo extends Component {
       }
       const {rptedu, rptexp, rptpolitics} = CEC.data[id];
 
+      //如果有政見，計算政見最長字串，如果小於 15，改用置中
+      let politicsClass = styles.sectionWrap;
+      if(rptpolitics){
+          let paras = rptpolitics.split("<br/>");
+          let shortPara = true;
+          let i = 0;
+          for(i=0;i<paras.length;i++){
+            if(paras[i].length > 15){
+                shortPara = false;
+            }
+          }
+          if(shortPara){
+            politicsClass = styles.centerWrap;
+          }
+          
+      }
+
+
       return (
         <div className={styles.wrap}>
-            <div className={styles.section}>
-                <div className={styles.titleWrap}><h2 className={styles.title}>學歷</h2></div>
-                <div className={styles.centerWrap} dangerouslySetInnerHTML={{__html: rptedu}}/>
-            </div>
-            <div className={styles.section}>
-              <div className={styles.titleWrap}><h2 className={styles.title}>經歷</h2></div>
-              <div className={styles.centerWrap} dangerouslySetInnerHTML={{__html: rptexp}}/>
-            </div>
-            <div className={styles.section}>
-              <div className={styles.titleWrap}><h2 className={styles.title}>政見</h2></div>
-              <div className={styles.sectionWrap} dangerouslySetInnerHTML={{__html: rptpolitics}}/>
+            <div className={styles.paper}>
+                <div className={styles.section}>
+                    <div className={styles.titleWrap}><h2 className={styles.title}>學歷</h2></div>
+                    <div className={styles.centerWrap} dangerouslySetInnerHTML={{__html: rptedu}}/>
+                </div>
+                <div className={styles.section}>
+                  <div className={styles.titleWrap}><h2 className={styles.title}>經歷</h2></div>
+                  <div className={styles.centerWrap} dangerouslySetInnerHTML={{__html: rptexp}}/>
+                </div>
+                <div className={styles.section}>
+                  <div className={styles.titleWrap}><h2 className={styles.title}>政見</h2></div>
+                  <div className={politicsClass} dangerouslySetInnerHTML={{__html: rptpolitics}}/>
+                </div>
             </div>
             <div className={styles.meta}>* 候選人學經歷、政見資料來自
                 <a className={`${styles.ia} ${styles.black}`} 
